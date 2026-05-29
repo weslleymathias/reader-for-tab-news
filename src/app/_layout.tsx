05de1bb9e0efdbd7941cee3ea63d536cb36f49ef
Weslley Mathias <weslleymathias2006@hotmail.com>
2026-05-29 12:04:38 -0300

    feat: instala e configura expo router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,43 @@
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from "react";
+import { Stack } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+
+    const [loaded, error] = useFonts({
+        interRegular: require('./../shared/assets/Inter/Inter_18pt-Regular.ttf'),
+        interBold: require('./../shared/assets/Inter/Inter_18pt-Bold.ttf'),
+    })
+
+    useEffect(() => {
+        if (!loaded && !error) {
+            return;
+        }
+        SplashScreen.hide();
+    }, [loaded, error]);
+
+    if (!loaded && !error) return null;
+
+    return (
+        <View style={styles.container}>
+            <Text style={{ fontFamily: 'interBold' }}>Hello world!</Text>
+            <StatusBar style="auto" />
+
+            <Stack />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+});
