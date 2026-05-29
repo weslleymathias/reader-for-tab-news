8a08749fb2cd5ebbe21ab9ec817f2d9882e0ecba
Weslley Mathias <weslleymathias2006@hotmail.com>
2026-05-29 11:01:46 -0300

    feat: configura tokens do tema

---
+++ src/shared/themes/DarkTheme.ts
@@ -0,0 +1,22 @@
+export const DarkTheme = {
+    colors: {
+        border: '#3D444D',
+        text: '#F0F6FC',
+        background: '#15181D',
+        paper: '#151B23',
+        textMuted: '#9198A1',
+        highlight: '#388BFD',
+        textHighlight: '#388BFD',
+    },
+    fonts: {
+        families: {
+            regular: 'interRegular',
+            bold: 'interBold',
+        },
+        sizes: {
+            normal: 12,
+            small: 10,
+            large: 16,
+        },
+    }
+}
---
+++ src/shared/themes/LightTheme.ts
@@ -0,0 +1,22 @@
+export const LightTheme = {
+    colors: {
+        border: '#D0D7DE',
+        text: '#1F2328',
+        background: '#FFFFFF',
+        paper: '#F6F8FA',
+        textMuted: '#656D76',
+        highlight: '#0969DA',
+        textHighlight: '#0969DA',
+    },
+    fonts: {
+        families: {
+            regular: 'interRegular',
+            bold: 'interBold',
+        },
+        sizes: {
+            normal: 12,
+            small: 10,
+            large: 16,
+        },
+    }
+}
