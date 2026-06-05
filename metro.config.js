be2174f5ac93bad7a131bdb93806e63661412418
Weslley Mathias <weslleymathias2006@hotmail.com>
2026-06-05 15:56:33 -0300

    feat: adiciona punycode

--- metro.config.js
+++ metro.config.js
@@ -3,5 +3,10 @@ const { withNativewind } = require("nativewind/metro");
  
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+config.resolver.extraNodeModules = {
+  ...config.resolver.extraNodeModules,
+  punycode: require.resolve('punycode/'),
+};
  
 module.exports = withNativewind(config);
